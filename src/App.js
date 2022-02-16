9bbef567e85d45ba7793a3f58e657ddbc866798d
rlachivirus <rlachivirus@gmail.com>
2022-02-16 18:02:17 -0500

    added the functionality to add todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Weather() {
   return (
     <div>
+      SHOW WEATHER
+    </div>
+  )
+}
+
+function TodoForm(props) {
+  const [ todo, setTodo ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    props.addTodo(todo);
+    setTodo('');
+  }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Things to do</label>
+      <br/>
+      <input type='text' value={todo} onChange={handleChange}/>
+      <input type='submit' value='Add'/>
+    </form>
+  )
+}
+
+function TodoList(props) {
+  return (
+    <div>
+      <ul>
+        {props.lists.map((list, idx) => {
+            return <li key={`todo-${idx}`}>{list}</li>
+          })}
+      </ul>
+    </div>
+  )
+}
+
+function TodoDone() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function Calculator() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function App() {
+  const todoLists = [];
+
+  const [ lists, setLists ] = useState(todoLists);
+
+  const addTodo = (todo) => {
+    setLists([...lists, todo]);
+  }
+
+  return (
+    <div>
+      <Weather />
+      <TodoForm addTodo={addTodo}/>
+      <TodoList lists={lists}/>
+      <TodoDone />
+      <Calculator />
     </div>
   );
 }
