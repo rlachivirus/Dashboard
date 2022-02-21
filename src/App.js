7098febddf07fd4d02a1da7430574961543953be
rlachivirus <rlachivirus@gmail.com>
2022-02-20 23:52:29 -0500

    added the unique keys for map

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function Weather() {
   }, []);
 
   if ((!weather)) return null;
-  console.log(weather)
+  // console.log(weather)
   return (
     <div className='weather'>
       {/* <div className='forecast'> */}
         {weather.forecast.forecastday.map((weath) => {
           return (
-            <div className='forecast'>
+            <div key={weath.date} className='forecast'>
               <span className='forecast-date'>{weath.date}</span>
               <img className='forecast-img' src={weath.day.condition.icon} />
               <span className='forecast-temp'>{`${weath.day.mintemp_f} / ${weath.day.maxtemp_f}`}</span>
