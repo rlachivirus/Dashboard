d7f6043798a1aa0f3ae7dee04fe975139a474f3b
rlachivirus <rlachivirus@gmail.com>
2022-03-17 17:43:28 -0400

    persist List reordering

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,38 @@ function TodoList() {
   const [ iniData, setIniData ] = useState(initialData);
 
   const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
 
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = iniData.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...iniData,
+      columns: {
+        ...iniData.columns,
+        [newColumn.id]: newColumn,
+      }
+    }
+
+    setIniData(newState);
   }
 
   return (
