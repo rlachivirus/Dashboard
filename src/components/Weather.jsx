1c1c8792780914542843f2b466a7fae4f4973aaa
rlachivirus <rlachivirus@gmail.com>
2022-03-22 21:37:09 -0400

    destructured weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-function Weather(props) {
+function Weather({ checkedStatus }) {
     const [weather, setWeather] = useState(null);
     const [degreeType, setDegreeType] = useState('F')
 
@@ -18,33 +18,33 @@ function Weather(props) {
         };
     }
 
-    if ((!weather)) return <div className={props.checkedStatus ? 'loadingSign-dark' : 'loadingSign'}>Loading...</div>;
+    if ((!weather)) return <div className={checkedStatus ? 'loadingSign-dark' : 'loadingSign'}>Loading...</div>;
 
     let day = { '1': 'MONDAY', '2': 'TUESDAY', '3': 'WEDNESDAY', '4': 'THURSDAY', '5': 'FRIDAY', '6': 'SATURDAY', '0': 'SUNDAY' };
     let d = new Date();
     let dayNum = d.getDay();
 
     let showTemperature = degreeType === 'F' ? (
-        <p className={props.checkedStatus ? 'forecast-temperature-dark' : 'forecast-temperature'}>{weather.current.temp_f}°</p>
+        <p className={checkedStatus ? 'forecast-temperature-dark' : 'forecast-temperature'}>{weather.current.temp_f}°</p>
     ) : (
-        <p className={props.checkedStatus ? 'forecast-temperature-dark' : 'forecast-temperature'}>{weather.current.temp_c}°</p>
+        <p className={checkedStatus ? 'forecast-temperature-dark' : 'forecast-temperature'}>{weather.current.temp_c}°</p>
     )
 
     return (
-        <div className={props.checkedStatus ? 'weather-dark' : 'weather'}>
-            <p className={props.checkedStatus ? 'forecast-weekday-dark' : 'forecast-weekday'}>{day[dayNum]} • NEW YORK</p>
+        <div className={checkedStatus ? 'weather-dark' : 'weather'}>
+            <p className={checkedStatus ? 'forecast-weekday-dark' : 'forecast-weekday'}>{day[dayNum]} • NEW YORK</p>
             <div className='temperature-row'>
                 {showTemperature}
-                <p onClick={() => changeDegree()} className={props.checkedStatus ? 'dType-dark' : 'dType'}>{degreeType === 'F' ? 'F' : 'C'}</p>
+                <p onClick={() => changeDegree()} className={checkedStatus ? 'dType-dark' : 'dType'}>{degreeType === 'F' ? 'F' : 'C'}</p>
             </div>
-            <p className={props.checkedStatus ? 'forecast-condition-dark' : 'forecast-condition'}>{weather.current.condition.text.toUpperCase()}</p>
+            <p className={checkedStatus ? 'forecast-condition-dark' : 'forecast-condition'}>{weather.current.condition.text.toUpperCase()}</p>
             <img className='forecast-image' src={weather.current.condition.icon} />
             {/* <div className='forecast-weather'>
-                <span className={props.checkedStatus ? 'forecast-circles-dark' : 'forecast-circles'}>
+                <span className={checkedStatus ? 'forecast-circles-dark' : 'forecast-circles'}>
                     <p>{day[dayNum + 1].slice(0, 3)}</p>
                     <p>{degreeType === 'F' ? weather.forecast.forecastday[1].day.avgtemp_f : weather.forecast.forecastday[1].day.avgtemp_c }</p>
                 </span>
-                <span className={props.checkedStatus ? 'forecast-circles-dark' : 'forecast-circles'}>
+                <span className={checkedStatus ? 'forecast-circles-dark' : 'forecast-circles'}>
                     <p>{day[dayNum + 2].slice(0, 3)}</p>
                     <p>{degreeType === 'F' ? weather.forecast.forecastday[2].day.avgtemp_f : weather.forecast.forecastday[2].day.avgtemp_c}</p>
                 </span>
