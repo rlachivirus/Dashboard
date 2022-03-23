8b94071d8219894d98bae5bdfb7fc1da8ad22e2e
rlachivirus <rlachivirus@gmail.com>
2022-03-22 21:30:55 -0400

    destructured calculator and column components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function Calculator(props) {
+function Calculator({ checkedStatus }) {
 
     const [firstNum, setFirstNum] = useState('0');
     const [secondNum, setSecondNum] = useState('');
@@ -110,36 +110,36 @@ function Calculator(props) {
     }
 
     return (
-        <div className={props.checkedStatus ? 'calculator-dark' : 'calculator'}>
-            <span className={props.checkedStatus ? 'calculator-result-dark' : 'calculator-result'}>{result ? result : secondNum.length === 0 ? firstNum : secondNum}</span>
+        <div className={checkedStatus ? 'calculator-dark' : 'calculator'}>
+            <span className={checkedStatus ? 'calculator-result-dark' : 'calculator-result'}>{result ? result : secondNum.length === 0 ? firstNum : secondNum}</span>
             <div className='row'>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>AC</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>+/-</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>%</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>÷</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>AC</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>+/-</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>%</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>÷</span>
             </div>
             <div className='row'>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>7</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>8</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>9</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>×</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>7</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>8</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>9</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>×</span>
             </div>
             <div className='row'>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>4</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>5</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>6</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>-</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>4</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>5</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>6</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>-</span>
             </div>
             <div className='row'>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>1</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>2</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>3</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>+</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>1</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>2</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>3</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleSign}>+</span>
             </div>
             <div className='row'>
-                <span className={props.checkedStatus ? 'num-zero-dark' : 'num-zero'} onClick={handleNumbers}>0</span>
-                <span className={props.checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>.</span>
-                <span className={props.checkedStatus ? 'icons-dark' : 'icons'} onClick={handleResult}>=</span>
+                <span className={checkedStatus ? 'num-zero-dark' : 'num-zero'} onClick={handleNumbers}>0</span>
+                <span className={checkedStatus ? 'numbers-dark' : 'numbers'} onClick={handleNumbers}>.</span>
+                <span className={checkedStatus ? 'icons-dark' : 'icons'} onClick={handleResult}>=</span>
             </div>
         </div>
     )
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -2,23 +2,23 @@ import { Droppable } from 'react-beautiful-dnd';
 import TodoForm from './TodoForm';
 import Task from './Task'
 
-function Column(props) {
+function Column({ checkedStatus, column, iniData, addTodo, tasks }) {
     return (
-        <div className={props.checkedStatus ? 'todos-dark' : 'todos'}>
-            <div className={props.checkedStatus ? 'column-header-dark' : 'column-header'}>
+        <div className={checkedStatus ? 'todos-dark' : 'todos'}>
+            <div className={checkedStatus ? 'column-header-dark' : 'column-header'}>
                 <div className='empty-placeholder'> </div>
-                <p className='column-title'>{props.column.title}</p>
-                <TodoForm title={props.column.title} iniData={props.iniData} addTodo={props.addTodo} checkedStatus={props.checkedStatus} />
+                <p className='column-title'>{column.title}</p>
+                <TodoForm title={column.title} iniData={iniData} addTodo={addTodo} checkedStatus={checkedStatus} />
             </div>
-            <Droppable droppableId={props.column.id}>
+            <Droppable droppableId={column.id}>
                 {provided => (
                     <ul
                         className='todo-lists'
                         ref={provided.innerRef}
                         {...provided.droppableProps}
                     >
-                        {props.tasks.map((task, index) => (
-                            <Task key={task.id} task={task} index={index} checkedStatus={props.checkedStatus} column={props.column} />
+                        {tasks.map((task, index) => (
+                            <Task key={task.id} task={task} index={index} checkedStatus={checkedStatus} column={column} />
                         ))}
                         {provided.placeholder}
                     </ul>
